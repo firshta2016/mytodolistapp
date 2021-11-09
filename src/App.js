54b58116c82c5d93f913ac1373683b5a2575439d
Firshta Shefa <fshefa@bellevuewa.gov>
2021-11-08 20:44:06 -0800

    pushing all the work at once

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
 import React, { useState } from "react";
 import "./App.css";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
 
-function App() {
-  const todoData = [
-    {
-      id: 1,
-      item: "Apple",
-    },
-    {
-      id: 2,
-      item: "Computer",
-    },
-    {
-      id: 3,
-      item: "Paper",
-    },
-    {
-      id: 4,
-      item: "Love",
-    },
-  ];
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
 
-  const deleteTaskItems = (event) => {
-    const element = event.target;
-    element.classList.toggle("done");
-  };
+  function addTask(name) {
+    const newTask = { id: "id", name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
 
+  function toggleTaskCompleted(id) {
+    const updatedTask = tasks.map((task) => {
+      if (id === tasks.id) {
+        return { ...tasks, completed: !tasks.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTask);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   return (
-    <div>
-      <h1>Todo List</h1>
-
-      <ul>
-        {todoData.map((items) => {
-          return <li onClick={deleteTaskItems}>{items.item}</li>;
+    <div className="todoapp stack-large">
+      <Form addTask={addTask} />
+      <h1>Todo</h1>
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {tasks.map((items) => {
+          return (
+            <Todo
+              name={items.name}
+              completed={items.completed}
+              id={items.id}
+              key={items.id}
+              toggleTaskCompleted={toggleTaskCompleted}
+              deleteTask={deleteTask}
+              editTask={editTask}
+            />
+          );
         })}
       </ul>
-
-      <form>
-        <input type="text" />
-        <button>Delete</button>
-        <button onClick={() => console.log("button works")}>Edit</button>
-      </form>
     </div>
   );
 }
 
 export default App;
-
 /**
  * create the data
  * loop through the data and output to the UI
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: false },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+  { id: "todo-3", name: "Party", completed: true },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>,
   document.getElementById("root")
 );
